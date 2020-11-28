6375e8ba6c6f2f7604e389f8b69a05c7ee63a7c1
mohamed-ahmed527 <71071161+mohamed-ahmed527@users.noreply.github.com>
2020-11-28 22:42:39 +0200

    education-website

---
+++ education-Js.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('nav span').click(function(){
+       $('nav .list').slideToggle();              
+    });
+    $('.drop1 i').click(function(){
+       $('.sub-menu-1').slideToggle(); 
+    });
+    
+    
+    
+    
+    
+});
+                  
